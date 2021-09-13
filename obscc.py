6877db8a8025d2b2eaf6c897a96070e4beaef4a9
znsoft <znsoft@163.com>
2021-09-13 15:28:08 +0800

    update files.

--- obscc.py
+++ obscc.py
@@ -1,8 +1,25 @@
 import torch 
 from znprompt import znprompt as zp
+from torch.utils.data import DataLoader,Dataset
+import torch.nn.functional as F
+import os
+import shutil
+from pathlib import Path 
+from torchsummaryX import summary as summaryX
+import onnx 
+import pickle as pk 
+from  traininfo import TrainInfo
 
 
+'''
 zpobj=zp()
 
 zpobj.finish()
 zpobj.error()
+
+'''
+
+
+DATA_DIR="/data/linux/scc/files"
+TRAIN_DIR="train"
+VALID_DIR="valid"
