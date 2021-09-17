442e4c730319730d4452c5b7f798d0f5a3e7eb09
znsoft <znsoft@163.com>
2021-09-18 07:30:23 +0800

    update files.

--- obscc.py
+++ obscc.py
@@ -13,6 +13,7 @@ import json
 import threading 
 import math 
 import re
+import time
 '''
 zpobj=zp()
 
@@ -33,21 +34,44 @@ TRAIN_LIST="train.lst"
 VALID_LIST="valid.lst"
 TEST_LIST="test.lst"
 
-CAT_FILE="languages.json"
+JSON_FILE="languages.json"
+
+MODEL_NAME="src_cat.pth"
+ONNX_MODEL_PATH="src_cat.onnx"
+
+DATASET_FILE="ds.dat"
+VOCAB_FILE="vocab.dat"
+CAT_FILE="allcat.dat"
+
+DATAFILE_PREFIX="ds_{}_{}.dat"
+VOCABFILE_PREFIX="vocab_{}.dat"
 
 THREAD_NUM=32
+
+MAX_TOKEN =1000
+
+MIN_WORD_FREQUENCE=3
+
+LASTPART=-1  # -1 normal, 100000 test only
+
+def save_var(varname,filename):
+    with open(filename,"wb") as f:
+        pk.dump(varname,f)
+
+def load_var(filename):
+    with open(filename,"rb") as f:
+        return pk.load(f)
 
 
 
 class ZnPreprocess(threading.Thread):
-    def __init__(self,DataDir,DstDir,threadid,threadnum):
+    def __init__(self,DataDir,DstDir,threadid):
         super(ZnPreprocess,self).__init__()
         self.src=DataDir
         self.dst=DstDir
 
         self.threadid=threadid
-        self.threadnum=threadnum
-
+      
         if threadid ==0:
             if Path(self.dst).exists():
                 shutil.rmtree(self.dst,ignore_errors=True)
@@ -80,9 +104,9 @@ class ZnPreprocess(threading.Thread):
 
         for i in range(len(dirs)):
                 
-            self.filenumber=math.ceil(len(filelists[i])/self.threadnum)
+            self.filenumber=math.ceil(len(filelists[i])/THREAD_NUM)
 
-            if(self.threadnum == self.threadid+1 ):
+            if(THREAD_NUM == self.threadid+1 ):
                 self.filelist=filelists[i][self.threadid*self.filenumber:]
             else:    
                 self.filelist=filelists[i][self.threadid*self.filenumber:(self.threadid+1)*self.filenumber]
@@ -107,38 +131,190 @@ class ZnPreprocess(threading.Thread):
 
 
 
+def strip_chinese(strs):
+    if strs.find("STRSTUFF") > -1 and len(strs)>8:
+        print(strs)
+    for _char in strs:
+        if '\u4e00' <= _char <= '\u9fa5':
+            return ""
+    return strs
+
+# get the list of unique vocab
+class ZnQmProcessData(threading.Thread):
+    def __init__(self,DataDir,catfile,filelist,threadid,vocab,bGetVocab):
+          
+        with open(DataDir+os.sep+".."+os.sep+filelist,"r") as f:
+            self.allfiles=f.readlines()
+
+        self.allfiles=self.allfiles[:LASTPART] # debug only
+        self.datadir=DataDir+os.sep+TRAIN_DIR
+        self.catfile=DataDir+os.sep+".."+os.sep+catfile
+        self.threadid=threadid  
+        self.vocab=vocab
+        self.vocab_dict={}
+        self.RungetVocab=bGetVocab
+    
+    def run(self):
+
+        if self.RungetVocab:
+            self.getvocab()
+        else:
+            self.gettraininfo()
+
+    def getvocab(self):
+        allcat={}
+        if self.threadid == 0:
+            with open(self.catfile,"r") as f:
+                catlist=json.load(f)
+                for id,item in enumerate(catlist.values()):
+                    allcat[item]=id
+            save_var(allcat,CAT_FILE)
+
+        self.filenumber=math.ceil(len(self.allfiles)/THREAD_NUM)
+
+        if(THREAD_NUM == self.threadid+1 ):
+            filelist=self.allfiles[self.threadid*self.filenumber:]
+        else:    
+            filelist=self.allfiles[self.threadid*self.filenumber:(self.threadid+1)*self.filenumber]
+
+        for file in filelist:
+            file=file.strip()
+            fullpath=self.datadir+os.sep+file 
+            with open(fullpath,"r",encoding="utf-8") as f:
+                lines= f.readlines()  
+                lines=list(map(lambda x:x.replace("\n",""),lines))
+                lines=list(map(strip_chinese,lines))
+
+                newlines=[token   for token in lines if token != '' and token !=' ']
+
+                for item in newlines:
+                    if item in self.vocab_dict:
+                        self.vocab_dict[item]+=1
+                    else:
+                        self.vocab_dict[item]=1
+        self.new_vocab=set()
+        #self.new_vocab[""]=0
+        for item in self.vocab_dict:
+            if self.vocab_dict[item] >MIN_WORD_FREQUENCE:
+                self.new_vocab.update(item)
+              
+        
+        filename=VOCABFILE_PREFIX.format(self.threadid)
+        save_var(self.new_vocab,filename)
+
+        #VOCAB.update(new_vocab.keys()) # 赋值
+        #VOCAB.add("") #添加空字符
+  
+    def gettraininfo(self):
+
+        self.filenumber=math.ceil(len(self.allfiles[i])/THREAD_NUM)
+
+        if(THREAD_NUM == self.threadid+1 ):
+            self.filelist=self.allfiles[i][self.threadid*self.filenumber:]
+        else:    
+            self.filelist=self.allfiles[i][self.threadid*self.filenumber:(self.threadid+1)*self.filenumber]
+        lines=[]
+        vocab_dict={}
+        for file in self.allfiles:
+            file=file.strip()
+            fullpath=self.datadir+os.sep+file 
+            with open(fullpath,"r",encoding="utf-8") as f:
+                lines= f.readlines()  
+                lines=list(map(lambda x:x.replace("\n",""),lines))
+                lines=list(map(strip_chinese,lines))
+
+                newlines=[token   for token in lines if token != '' and token !=' ']
+
+                for item in newlines:  # count the frequence of words
+                    if item in vocab_dict:
+                        vocab_dict[item]+=1
+                    else:
+                        vocab_dict[item]=1
+
+                nLines=len(newlines)
+                if  nLines <MAX_TOKEN :
+                    newlines.extend([""]*(MAX_TOKEN-nLines))
+                else:
+                    newlines=newlines[:MAX_TOKEN]
+
+                theline=[self.vocab[item] if item in self.vocab else 0  for item in newlines]
+                lines.append(theline)
+        length=len(lines)
+
+        filename=DATAFILE_PREFIX.format(self.threadid,length)
+        save_var(lines,filename)
+
+        
 class ZnQmDataset(Dataset):
-    def __init__(self,DataDir,catfile,WORD_LIST):
+    def __init__(self,DataDir,filelist,catfile,VOCAB):
         super(ZnQmDataset,self).__init__()
         self.allcat={}
-        self.data_x=[]
-        self.data_y={}
-        with open(catfile,"r") as f:
-            catlist=json.load(f)
-            for id,item in enumerate(catlist.values()):
-                self.allcat[item]=id
-            
+        self.x_data=[]
+        self.y_data=[]
+        self.filelist=filelist
+        self.vocab_dict={}
+
                 
-            with open(DATA_DIR+os.sep+TRAIN_LIST,"r") as f:
-                allfiles=f.readlines()
-            
+        with open(DATA_DIR+os.sep+TRAIN_LIST,"r") as f:
+            allfiles=f.readlines()
+        
+            #for debug only
+            # allfiles=allfiles[:100]
             for file in allfiles:
                 file=file.strip()
                 fullpath=DataDir+os.sep+os.sep+file 
-                #print(fullpath)
+                with open(fullpath,"r",encoding="utf-8") as f:
+                    lines= f.readlines()  
+                    lines=list(map(lambda x:x.replace("\n",""),lines))
+                    lines=list(map(strip_chinese,lines))
 
+                    newlines=[token   for token in lines if token != '' and token !=' ']
+       
+                    for item in newlines:
+                        if item in self.vocab_dict:
+                            self.vocab_dict[item]+=1
+                        else:
+                            self.vocab_dict[item]=1
+
+                    nLines=len(newlines)
+                    if  nLines <MAX_TOKEN :
+                        newlines.extend([""]*(MAX_TOKEN-nLines))
+                    else:
+                        newlines=newlines[:MAX_TOKEN]
+
+                                     
+                    for item in newlines:
+                        if item in self.vocab_dict:
+                            self.vocab_dict[item]+=1
+                        else:
+                            self.vocab_dict[item]=1
+
+                    nLines=len(newlines)
+                    if  nLines <MAX_TOKEN :
+                        newlines.extend([""]*(MAX_TOKEN-nLines))
+                    else:
+                        newlines=newlines[:MAX_TOKEN]
+
+                    self.x_data.append(newlines)
+                    self.y_data.append(self.allcat[str(Path(fullpath).suffix)[1:]])
+        self.y_data=torch.tensor(self.y_data)
+
+       
+
+
+       
     
     def __len__(self):
-        pass
+        return len (self.y_data)
     
     def __getitem__(self, index):
-        pass
+        return self.x_data[index],self.y_data[index]
 
 
 
 
 
-def doTrain(ds,vocab):
+def doTrain(ds,WORD_LIST):
     pass
 
 if __name__=="__main__":
@@ -155,7 +331,36 @@ if __name__=="__main__":
         for subthread in threads:
             subthread.join()
 
-    WORD_LIST=dict()
+#  VOCAB=set()
 
-    ds=ZnQmDataset(DATA_DIR+os.sep+ORI_DIR+os.sep+TRAIN_DIR,DATA_DIR+os.sep+CAT_FILE,WORD_LIST)
-    doTrain(ds,WORD_LIST)
+#   if os.path.exists(VOCAB_FILE):
+#        VOCAB=load_var(VOCAB_FILE)
+
+
+
+    
+    bGenData=True
+    if(bGenData):
+        vocab={}
+        for i in range(THREAD_NUM):
+            dp=ZnQmProcessData(DATA_DIR+os.sep+DST_DIR,JSON_FILE,TRAIN_LIST,i,vocab,True)
+            dp.getvocab()
+
+
+    if(os.path.exists(DATASET_FILE)):
+        ds=load_var(DATASET_FILE)
+    else:
+        ds=ZnQmDataset(DATA_DIR+os.sep+DST_DIR+os.sep+TRAIN_DIR,DATA_DIR+os.sep+TRAIN_LIST,DATA_DIR+os.sep+JSON_FILE,VOCAB)
+        save_var(VOCAB,VOCAB_FILE)
+        save_var(ds,DATASET_FILE)
+
+  
+
+    WORDLIST={key:i for i,key in enumerate(VOCAB)}
+
+  
+    zpobj=zp()
+    doTrain(ds,WORDLIST)
+
+    zpobj.finish()
+    print("done!!!")
